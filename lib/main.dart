04eab4b14393b9db91d2790e181c77b094ef2f71
Poomrapee Patum <150318016+Daijirew@users.noreply.github.com>
2024-11-04 23:40:02 +0700

    Firebase Bugfix
    11/4/2024

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myproject/pages.dart/buttomnav.dart';
+import 'package:myproject/pages.dart/home.dart';
+import 'package:myproject/pages.dart/login.dart';
+import 'package:myproject/pages.dart/onboard.dart';
+import 'package:myproject/pages.dart/sigup.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LogIn(),
+    );
+  }
+}
